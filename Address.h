501cc30708f5c8470413527d3735ff9402c18c81
Craig Buchanan <craibuc@gmail.com>
2014-04-10 14:04:10 -0500

    Added implementation specifics

--- Address.h
+++ Address.h
@@ -8,6 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Address : NSObject
+@interface Address : NSObject {}
+
+@property (copy) NSString *city;
+@property (copy) NSString *regionCode;
+@property (copy) NSString *postalCode;
+@property (copy) NSString *countryCode;
+
+@property double latitude;
+@property double longitude;
+
+-(id) initWithData:(NSString*) aCity
+                  :(NSString*) aRegionCode
+                  :(NSString*) aPostalCode
+                  :(NSString*) aCountryCode
+                  :(double) aLatitude
+                  :(double) aLongitude;
 
 @end
--- Address.m
+++ Address.m
@@ -10,4 +10,38 @@
 
 @implementation Address
 
+-(id) initWithData:(NSString*) aCity
+                  :(NSString*) aRegionCode
+                  :(NSString*) aPostalCode
+                  :(NSString*) aCountryCode
+                  :(double) aLatitude :(double) aLongitude {
+
+    self = [super init];
+    
+    if (self) {
+        
+        [self setCity: aCity];
+        [self setRegionCode: aRegionCode];
+        [self setPostalCode: aPostalCode];
+        [self setCountryCode: aCountryCode];
+        
+        [self setLatitude: aLatitude];
+        [self setLongitude: aLongitude];
+        
+    }
+    
+    return self;
+    
+}
+
+- (void)dealloc {
+    
+    [_city release];
+	[_regionCode release];
+    [_postalCode release];
+	[_countryCode release];
+    
+    [super dealloc];
+}
+
 @end
